639b406cd768b4984ea52f011f7166ae354e5371
weterynarzfred <weterynarzfred@gmail.com>
2024-06-27 15:28:13 +0200

    change feed id

--- parts/instagram.php
+++ parts/instagram.php
@@ -1,16 +1,15 @@
 <?php
-return;
-
-$link = get_field('instagram_link', get_option('page_on_front'));
+$contact = get_field('contact', get_option('page_on_front'));
 ?>
 
 <div class="content-wrap">
   <div id="instagram-feed" class="content column">
-    <?php echo do_shortcode('[instagram-feed]'); ?>
+    <?php echo do_shortcode('[instagram-feed feed=2]'); ?>
   </div>
   <div class="r"></div>
   <div class="flex flex-justify-center">
-    <div class="text large uppercase"><a href="<?= $link ?>" target="_blank"
+    <div class="text large uppercase"><a
+        href="<?= $contact['instagram_link'] ?>" target="_blank"
         rel="noopener noreferrer"><u>@wesola.cafe</u></a></div>
   </div>
   <div class="rsep"></div>

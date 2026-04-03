ee085ef6aa276bcf1efb917fa317b8892cb98ec3
weterynarzfred <weterynarzfred@gmail.com>
2026-04-03 16:16:47 +0200

    update menu page look

--- include/enqueue.php
+++ include/enqueue.php
@@ -1,9 +1,10 @@
 <?php
 add_action('init', function () {
+  $version = "1.1";
   $url = get_template_directory_uri() . '/dist/';
-  wp_register_style('widok-main', $url . 'main.css', [], 1.0);
+  wp_register_style('widok-main', $url . 'main.css', [], $version);
   // wp_register_style('widok-templates', $url . 'templates.css', [], 1.0);
-  wp_register_script('widok-main', $url . 'main.js', [], 1.0, true);
+  wp_register_script('widok-main', $url . 'main.js', [], $version, true);
 });
 
 // add_action('admin_init', function () {
--- templates/menu.php
+++ templates/menu.php
@@ -11,30 +11,46 @@ $menu_date_pl = $menu_pl->format('Ymd');
 $menu_en      = new DateTimeImmutable($eating_en['menu_download']['modified']);
 $menu_date_en = $menu_en->format('Ymd');
 
+$texts = get_field('texts');
+
 ?>
 
 <div class="rsep rsep--top"></div>
 
 <div class="content-wrap">
   <div class="content column">
-    <div class="accent"><?= get_the_title() ?></div>
+    <div class="eating-img-2">
+      <img src="<?= get_template_directory_uri() ?>/media/img-2.svg" alt="">
+    </div>
+    <div class="accent"><?= $texts['title'] ?></div>
+    <div class="text uppercase large"><?= $texts['subtitle'] ?></div>
+    <div class="r"></div>
     <div class="r"></div>
     <div class="flex" id="menu-flex">
       <a href="<?= home_url('menu-pl') . '?t=_' . $menu_date_pl ?>"
         target="_blank" rel="noopener noreferrer" class="menu-download">
-        <div class="menu-spacer"></div>
         <div class="menu-text">
-          <?= get_field('polish', pll_get_post(get_the_ID(), 'pl')) ?>
+          <div><?= get_field('polish', pll_get_post(get_the_ID(), 'pl')) ?></div>
+          <img
+            class="menu-download-icon"
+            src="<?= get_template_directory_uri() ?>/media/icon-arrow.svg"
+            alt="download">
         </div>
       </a>
       <a href="<?= home_url('menu-en') . '?t=_' . $menu_date_en ?>"
         target="_blank" rel="noopener noreferrer" class="menu-download">
-        <div class="menu-spacer"></div>
         <div class="menu-text">
-          <?= get_field('english', pll_get_post(get_the_ID(), 'pl')) ?>
+          <div><?= get_field('english', pll_get_post(get_the_ID(), 'pl')) ?></div>
+          <img
+            class="menu-download-icon"
+            src="<?= get_template_directory_uri() ?>/media/icon-arrow.svg"
+            alt="download">
         </div>
       </a>
     </div>
+    <div class="r"></div>
+    <div class="r"></div>
+    <div class="text menu-description"><?= $texts['description'] ?></div>
   </div>
 </div>
 
